2d15dd02e8c3d17a87d735d036122084b6284266
ZuggieTM <zuggiee@gmail.com>
2018-10-03 15:06:07 +0200

    Update todolist.blade.php

--- src/views/todolist.blade.php
+++ src/views/todolist.blade.php
@@ -1,3 +1,7 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
 <style>
     .card {
         max-width: 800px;
@@ -15,7 +19,15 @@
     i {
         cursor: pointer;
     }
-
+    .controls {
+        float: right;
+    }
+    .editTodo {
+        margin-right: 10px;
+    }
+    .deleteTodo {
+        color: red;
+    }
 </style>
 <div class="card">
     <div class="todo-header d-flex align-items-center justify-content-between">
@@ -26,11 +38,11 @@
     </div>
     <div class="card-body">
         <ul class="list-group">
-          <li class="list-group-item"><span>Køb ind til aftensmad</span><div class="controls" style="float:right;"><i class="fas fa-edit editTodo" style="margin-right:10px" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo" style="color:red;"></i></div></li>
-          <li class="list-group-item"><span>Tænd for spaen</span><div class="controls" style="float:right;"><i class="fas fa-edit editTodo" style="margin-right:10px" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo" style="color:red;"></i></div></li>
-          <li class="list-group-item"><span>Løs en rubiks terning</span><div class="controls" style="float:right;"><i class="fas fa-edit editTodo" style="margin-right:10px" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo" style="color:red;"></i></div></li>
-          <li class="list-group-item"><span>Klap din hund på hovedet</span><div class="controls" style="float:right;"><i class="fas fa-edit editTodo" style="margin-right:10px" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo" style="color:red;"></i></div></li>
-          <li class="list-group-item"><span>Hop på ét ben i 120 sekunder</span><div class="controls" style="float:right;"><i class="fas fa-edit editTodo" style="margin-right:10px" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo" style="color:red;"></i></div></li>
+          <li class="list-group-item"><span>Køb ind til aftensmad</span><div class="controls"><i class="fas fa-edit editTodo" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo"></i></div></li>
+          <li class="list-group-item"><span>Tænd for spaen</span><div class="controls"><i class="fas fa-edit editTodo" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo"></i></div></li>
+          <li class="list-group-item"><span>Løs en rubiks terning</span><div class="controls"><i class="fas fa-edit editTodo" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo"></i></div></li>
+          <li class="list-group-item"><span>Klap din hund på hovedet</span><div class="controls"><i class="fas fa-edit editTodo" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo"></i></div></li>
+          <li class="list-group-item"><span>Hop på ét ben i 120 sekunder</span><div class="controls"><i class="fas fa-edit editTodo" data-toggle="modal" data-target="#todo-modal"></i><i class="fas fa-trash deleteTodo"></i></div></li>
         </ul>
     </div>
 </div>
@@ -92,3 +104,5 @@
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
